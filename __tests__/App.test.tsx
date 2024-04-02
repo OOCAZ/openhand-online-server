6e7dec06cbe6ee5a2621b89d99fee08ff033a996
Zac Poorman <zpoorman@us.ci.org>
2024-04-02 10:36:04 -0600

    fixed unit test by mocking axios

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,6 +3,8 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import App from "../src/app/page";
 import React from "react";
 
+jest.mock("axios");
+
 describe("Page", () => {
   it("renders a heading", () => {
     render(<App />);
