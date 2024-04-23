cf0f29c8135837355e45d0d8573902c2e9818732
Zac Poorman <poortexanlegos@gmail.com>
2024-04-22 19:01:04 -0600

    added connection to the other api endpoints

--- src/pages/api/add.js
+++ src/pages/api/add.js
@@ -3,7 +3,17 @@ import mongoose from "mongoose";
 
 export default async function handler(req, res) {
   try {
-    if (mongoose.connection.readyState) {
+    if (!mongoose.connection.readyState) {
+      const session = await mongoose
+        .connect(process.env.DATABASE_ACCESS)
+        .then(async function (models) {
+          let numbers = mongoose.model("mytable");
+          const numbers2 = await numbers.find();
+          res.status(200).send(numbers2);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
       console.log(req.body);
       const numbersInit = new numbersTemplateCopy({
         number: req.body.number,
@@ -16,8 +26,16 @@ export default async function handler(req, res) {
         })
         .catch((err) => res.json(err));
     } else {
-      console.log("Connection to database failed");
-      res.status(200).send("Connection to database failed");
+      const numbersInit = new numbersTemplateCopy({
+        number: req.body.number,
+      });
+      numbersInit
+        .save()
+        .then((data) => {
+          console.log("Number added");
+          res.status(200).json(data);
+        })
+        .catch((err) => res.json(err));
     }
   } catch (err) {
     console.log(err);
--- src/pages/api/remove.js
+++ src/pages/api/remove.js
@@ -6,7 +6,18 @@ export default async function handler(req, res) {
   const numbersInit = new numbersTemplateCopy({
     number: req.body.number,
   });
-  if (mongoose.connection.readyState) {
+  if (!mongoose.connection.readyState) {
+    //connection logic
+    const session = await mongoose
+      .connect(process.env.DATABASE_ACCESS)
+      .then(async function (models) {
+        let numbers = mongoose.model("mytable");
+        const numbers2 = await numbers.find();
+        res.status(200).send(numbers2);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
     numbersInit.collection
       .deleteMany({ number: req.body.number })
       .then((data) => {
@@ -15,7 +26,12 @@ export default async function handler(req, res) {
       })
       .catch((err) => res.json(err));
   } else {
-    console.log("Connection to database failed");
-    res.status(200).send("Connection to database failed");
+    numbersInit.collection
+      .deleteMany({ number: req.body.number })
+      .then((data) => {
+        console.log("Number removed");
+        res.status(200).json(data);
+      })
+      .catch((err) => res.json(err));
   }
 }
